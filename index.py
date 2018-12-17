2022352081b728b86f42f44050b74f6e0b38c609
knguyen525 <khanh@cloudinox.com>
2018-12-17 00:00:23 -0800

    Update index.py

--- index.py
+++ index.py
@@ -4,9 +4,19 @@ import datetime
 
 def handler(event, context):
     data = {
-        'output': 'Hello World from MS1',
+        'output': 'Hello World from MS1 - handler',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
             'body': json.dumps(data),
             'headers': {'Content-Type': 'application/json'}}
+
+def handler2(event, context):
+    data = {
+        'output': 'Hello World from MS1 - handler2',
+        'timestamp': datetime.datetime.utcnow().isoformat()
+    }
+    return {'statusCode': 200,
+            'body': json.dumps(data),
+            'headers': {'Content-Type': 'application/json'}}
+
