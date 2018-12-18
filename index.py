eca7945ef60d3714c90636e84a38176886a1cf4e
Khanh Nguyen <khanh@cloudinox.com>
2018-12-18 11:15:53 -0800

    no message

--- index.py
+++ index.py
@@ -10,13 +10,3 @@ def handler(event, context):
     return {'statusCode': 200,
             'body': json.dumps(data),
             'headers': {'Content-Type': 'application/json'}}
-
-def handler2(event, context):
-    data = {
-        'output': 'Hello World from MS1 - handler2',
-        'timestamp': datetime.datetime.utcnow().isoformat()
-    }
-    return {'statusCode': 200,
-            'body': json.dumps(data),
-            'headers': {'Content-Type': 'application/json'}}
-
