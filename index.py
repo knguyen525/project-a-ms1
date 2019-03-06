8be3c989653d0083fb9de6174c054ff25cdc2f48
Khanh Nguyen <knguyen@nautilus.com>
2019-03-06 13:48:20 -0800

    sffasf

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import datetime
 
 def handler(event, context):
     data = {
-        'output': 'Hello World from MS1 - handler....develop pull request 4',
+        'output': 'Hello World from MS1 - handler....pull request 2',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
